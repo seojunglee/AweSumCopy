2120ceabcfe13258e3d539bc9658dfa2a8cea099
뽀야미 <ppoyami@ppoyamiui-MacBookPro.local>
2022-04-05 19:44:46 +0900

    중간 커밋

--- summary/views.py
+++ summary/views.py
@@ -1,13 +1,16 @@
-
 import imp
 import re
 
+
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from summarizer import Summarizer as BERT
+from summarizer.sbert import SBertSummarizer
+
 
 from transformers import pipeline
-#from simplet5 import SimpleT5
+
+
+
 
 
 from rest_framework.response import Response
@@ -23,17 +26,17 @@ from videos.models import Video
 @api_view(['get'])
 def showLongAPI(request, video_id):
     if video_id:
-        #요약 생성
-        model = BERT()
-        l_summary = LongSummary()
+        #요약 생성부
+        model = SBertSummarizer('paraphrase-MiniLM-L6-v2')
+        l_summary = LongSummary() #객체 생성
 
         this_video = get_object_or_404(Video, pk=video_id)
-        text = this_video.transcript   #텍스트 가져오기
+        text = this_video.transcript    #텍스트 가져오기
 
-        l_summary.body = model(text, min_length=60)    #요약문 generate
+        l_summary.body = model(text, num_sentences=15)      #요약문 generate
+    
         l_summary.video = this_video
         l_summary.save()  #저장 
-
 
         #해당 영상에 대한 summary 필터링 후 전달
         current_video = get_object_or_404(Video, pk=video_id)
@@ -50,33 +53,27 @@ def showLongAPI(request, video_id):
 
 
 
-
-
-
-
-
-
-
 ### MEDIUM SUMMARY 
 @api_view(['get'])
 def showMediumAPI(request, video_id):
-    m_summary = MediumSummary()  #객체생성
 
-    #요약 생성(long 불러와서 생성요약하기)
-    l_summary = get_object_or_404(LongSummary, pk=video_id)
+    m_summary = MediumSummary()  #객체생성
     
-    #model = SimpleT5()
-    #model.load_model("t5","outputs/SimpleT5-epoch-2-train-loss-0.9478", use_gpu=True)
-    #m_summary.body = model.predict(l_summary.body)  # msummary객체에 저장
-    
-    
-    
-    
+    #요약 생성(long 불러와서 생성요약하기) 
+    current_video = get_object_or_404(Video, pk=video_id)
+    l_summary = LongSummary.objects.filter(video = current_video)[0]
+
+
+
     
     
-    
+
+    m_summary.body = l_summary.body
+
     m_summary.video = l_summary.video
     m_summary.save()  #저장 
+
+
 
     #요약 전달
     current_video = get_object_or_404(Video, pk=video_id)
