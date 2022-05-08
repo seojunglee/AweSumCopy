021f1d4cd6192fe7c2de37c6102277428f887df1
뽀야미 <ppoyami@ppoyamiui-MacBookPro.local>
2022-05-09 00:25:58 +0900

    modified quiz model, serializer

--- quiz/models.py
+++ quiz/models.py
@@ -4,6 +4,7 @@ from videos.models import Video
 # Create your models here.
 
 class Quiz(models.Model):
+    quiz_num = models.IntegerField(default=1)
     question = models.TextField()
     answer = models.IntegerField()
     video = models.ForeignKey(Video, on_delete=models.CASCADE, null=True)
--- quiz/views.py
+++ quiz/views.py
@@ -17,7 +17,7 @@ def showQuiz(request):
 
     video_id = request.GET.get('id')
 
-    
+
     #퀴즈생성부
     current_video = get_object_or_404(Video, pk=video_id)
     bert = LongSummary.objects.filter(video = current_video)[0]
@@ -33,21 +33,21 @@ def showQuiz(request):
             this_quiz.answer = 1
             
         else: 
-            # 여기다가 < 거짓 문장으로 가공> 코드 삽입
+            #  < 거짓 문장으로 가공> 코드 삽입
             this_quiz.question = "False_Ex : " + each_line
             this_quiz.answer = 0
 
         this_quiz.video = bert.video
 
         if i != len(list)-1 :
-            #여기다가 < 문장을 퀴즈 형태로 가공 > 코드 삽입
+            #< 문장을 퀴즈 형태로 가공 > 코드 삽입
+            this_quiz.quiz_num=i+1
             this_quiz.save()
         i+=1
 
 
     
     #퀴즈보여주는부분
-    current_video = get_object_or_404(Video, pk=video_id)
     quizes = Quiz.objects.filter(video = current_video)
     serializer = QuizSerializer(quizes, many=True)
     return Response(serializer.data)
