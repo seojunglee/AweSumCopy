9331cd5b46b753d4951997b4f56580dafb8b4591
seojunglee <seojunglee1051@gmail.com>
2022-05-10 03:31:50 +0900

    Update settings.py

--- awesum/settings.py
+++ awesum/settings.py
@@ -149,3 +149,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_COOKIE_AGE = 1200
+SESSION_SAVE_EVERY_REQUEST = True
