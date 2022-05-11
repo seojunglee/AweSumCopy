9b023c359e1854e4ca695b3d65b20c8f04ae9a67
seojunglee <seojunglee1051@gmail.com>
2022-05-11 17:08:11 +0900

    Update views.py

--- summary/views.py
+++ summary/views.py
@@ -18,7 +18,7 @@ from .models import MediumSummary, LongSummary
 from .serializers import MSummarySerializer, LSummarySerializer
 
 from videos.models import Video
-
+import time
 
 ### LONG SUMMARY
 
@@ -29,23 +29,30 @@ def showLongAPI(request):
     
     if video_id:
         #요약 생성부
-        model = SBertSummarizer('paraphrase-MiniLM-L6-v2')
-        l_summary = LongSummary() 
 
-        this_video = get_object_or_404(Video, pk=video_id)
-        text = this_video.transcript    #텍스트 가져오기
+        time.sleep(10)
 
-        l_summary.body = model(text, num_sentences=15)      #요약문 generate
+
+
+
+
+        # model = SBertSummarizer('paraphrase-MiniLM-L6-v2')
+        # l_summary = LongSummary() 
+
+        # this_video = get_object_or_404(Video, pk=video_id)
+        # text = this_video.transcript    #텍스트 가져오기
+
+        # l_summary.body = model(text, num_sentences=15)      #요약문 generate
     
-        l_summary.video = this_video
-        l_summary.save()  #저장 
+        # l_summary.video = this_video
+        # l_summary.save()  #저장 
 
-        #해당 영상에 대한 summary 필터링 후 전달
-        current_video = get_object_or_404(Video, pk=video_id)
-        l_summary = LongSummary.objects.filter(video = current_video)
+        # #해당 영상에 대한 summary 필터링 후 전달
+        # current_video = get_object_or_404(Video, pk=video_id)
+        # l_summary = LongSummary.objects.filter(video = current_video)
 
-        serializer = LSummarySerializer(l_summary, many=True)
-        return Response(serializer.data)
+        # serializer = LSummarySerializer(l_summary, many=True)
+        return Response({"ok" : "ok"}})
 
 
 
